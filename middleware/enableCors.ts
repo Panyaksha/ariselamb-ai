59a445ac3da02c4a6ffd4ced5ef332da15d5f2cc
Muhammad Fatoni <panyaksha@gmail.com>
2025-11-28 04:28:45 +0700

    refactor: update CORS middleware to include 'uid' in allowedHeaders and improve comments

--- middleware/enableCors.ts
+++ middleware/enableCors.ts
@@ -3,9 +3,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 
 const cors = Cors({
-  origin: "*", // semua domain diizinkan
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept", "uid"],
 });
 
 function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
@@ -23,10 +23,8 @@ export function enableCors(
   handler: (req: NextApiRequest, res: NextApiResponse) => any
 ) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
-    // Jalankan CORS
     await runMiddleware(req, res, cors);
 
-    // Tambahan manual header agar Next.js aman
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -34,8 +32,12 @@ export function enableCors(
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Accept"
+      "Content-Type, Authorization, Accept, uid"
     );
+
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
 
     return handler(req, res);
   };
